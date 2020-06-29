ce669e7babc7daf207c60ab4635e8eefc298483c
JudeJ <YOUR_EMAIL_ADDRESS_HERE@users.noreply.github.com>
2020-06-29 09:26:05 -0700

    fixed extra comma

--- codeanalysis/binding/boundbinaryexpression.py
+++ codeanalysis/binding/boundbinaryexpression.py
@@ -3,7 +3,7 @@ from codeanalysis.binding.boundexpression import BoundExpression
 
 
 class BoundBinaryExpression(BoundExpression):
-    def __init__(self, left: BoundExpression, operator_kind: BoundBinaryOperatorKind, right: BoundExpression,) -> None:
+    def __init__(self, left: BoundExpression, operator_kind: BoundBinaryOperatorKind, right: BoundExpression) -> None:
         self.operator_kind = operator_kind
         self.left = left
         self.right = right
